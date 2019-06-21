cea7ff3c90b5ede7249ddf0487581ea099b684c1
Yiannis Siglidis <y.siglidis@gmail.com>
2019-06-21 19:54:42 +0200

    add deleted files

---
+++ .misc/test_random_dataset.py
@@ -0,0 +1,7 @@
+import hinrecodiv as nrd
+
+object_data=[('user',20),('item',20),('genre',5)]
+link_data=[('rates','user','item',0.05),('belongs','item','genre',0.1)]
+parameters={}
+
+a,b = nrd.datasets.random_hin_generator(object_data,link_data)
